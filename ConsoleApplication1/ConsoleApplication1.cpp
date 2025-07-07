7f6e0f77ca5ae2b235c15c93103602e94bccd203
Saso <msaso0725@gmail.com>
2025-07-07 14:23:46 +0200

    Some improvements

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+
+typedef std::vector<std::string> stringVector;
+
+#pragma region StatCheck
+std::string currName;
+int currDmg;
+int currHealth;
+int currMagic;
+stringVector currAbilities;
+#pragma endregion
+
 struct Character {
     std::string name;
-    int damage;
     int health;
+    int damage;
     int magic;
     std::vector<std::string> abilities;
 
@@ -24,19 +35,42 @@ static void choseCharacter() {
 
     int choice;
 
-    for (size_t i = 0; characters.size(); ++i) {
+    for (size_t i = 0;i < characters.size(); ++i) {
         std::cout << i + 1 << ", " << characters[i].name << std::endl;
     }
 
     std::cout << "Choose your character!" << std::endl;
 
     std::cin >> choice;
+
     if (choice >= 1 && choice <= characters.size()) {
-        std::cout << "Your choice is: ";
+        std::cout << "Your choice is: " << characters[choice - 1].name << std::endl;
+        std::cout << "Abilities: ";
         characters[choice - 1].display();
+        currDmg = characters[choice - 1].damage;
+        currHealth = characters[choice - 1].health;
+        currName = characters[choice - 1].name;
+        currMagic = characters[choice - 1].magic;
+        currAbilities = characters[choice - 1].abilities;
     }
 }
 int main()
 {
     std::cout << "Text based game" << std::endl;
+
+    choseCharacter();
+
+    #pragma region Unit Test for stat selection
+
+    std::cout << currName << "\n";
+    std::cout << currHealth << "\n";
+    std::cout << currDmg << "\n";
+    std::cout << currMagic << "\n";
+
+    for (size_t i = 0; i < currAbilities.size(); ++i) {
+        std::cout << currAbilities[i];
+    }
+    std::cout << std::endl;
+    #pragma endregion
+
 }
