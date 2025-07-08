07b28c03e43ac0d3a85807a2941d917049b193e6
Saso <msaso0725@gmail.com>
2025-07-08 10:29:34 +0200

    Some changes

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,12 +12,34 @@ int currMagic;
 stringVector currAbilities;
 #pragma endregion
 
+#pragma region EnemeyStats
+std::string name;
+int healt;
+int dmg;
+int magic;
+#pragma endregion
+
+
+struct Boss {
+    std::string name;
+    int health;
+    int dmg;
+    int magic;
+    stringVector abilities;
+};
+struct Enemy {
+    std::string name;
+    int healt;
+    int dmg;
+    int magic;
+};
+
 struct Character {
     std::string name;
     int health;
     int damage;
     int magic;
-    std::vector<std::string> abilities;
+    stringVector abilities;
 
     void display() const {
         for (const auto& ability : abilities) {
@@ -25,6 +47,23 @@ struct Character {
         }
         std::cout << std::endl;
     }
+};
+static void choseEnemy() {
+    std::vector<Enemy>enemies = {
+        {"Gbolin", 50, 5, 0},
+        {"Raider", 80, 8, 1},
+        {"Dog", 40, 10, 0},
+        {"Skeleton", 100, 5, 0}
+    };
+    int currentEnemy = rand() % enemies.size();
+    std::cout << "Enemey: " << std::endl;
+    std::cout << enemies[currentEnemy].name << ", " << enemies[currentEnemy].healt << ", " << enemies[currentEnemy].dmg << ", " << enemies[currentEnemy].magic;
+    std::cout << std::endl;
+
+    name = enemies[currentEnemy].name;
+    healt = enemies[currentEnemy].healt;
+    dmg = enemies[currentEnemy].dmg;
+    magic = enemies[currentEnemy].magic;
 };
 static void choseCharacter() {
     std::vector<Character> characters = {
@@ -54,14 +93,68 @@ static void choseCharacter() {
         currAbilities = characters[choice - 1].abilities;
     }
 }
+void fight() {
+    using namespace std;
+    int fightChoice;
+    int abilityChoice;
+    bool blockState = false;
+
+    std::string wantingToUse;
+
+    do
+    {
+        cout << "Your turn!" << "\n";
+        cout << "Choose your action!" << "\n";
+        cout << "1, Attack" << "\t" << "2, Block" << "\t" << "3, Use Ability" << "\t" << "4, Flee" << endl;
+        cin >> fightChoice;
+        switch (fightChoice)
+        {
+            case 1:
+                healt -= currDmg;
+                break;
+            case 2:
+                blockState = true;
+                break;
+            case 3:
+                cout << "Your abilites are: ";
+                for (const auto& abilty : currAbilities) {
+                    cout << abilty << ", ";
+                }
+                cout << endl;
+                cout << "Choose which spell you want to use: ";
+                cin >> abilityChoice;
+                wantingToUse = currAbilities[abilityChoice - 1];
+                useAbility(wantingToUse);
+                break;
+            case 4:
+                int flee = rand() % 2;
+                if (flee == 1) {
+                    cout << fleeFromCombat();
+                    break;
+                }
+                cout << "You couldnt flee";
+            default:
+                cout << "You did nothing! IDIOT" << endl;
+                break;
+        }
+    } while (healt > 0);
+}
+void useAbility(std::string s) {
+
+}
+std::string fleeFromCombat() {
+    return "You flee the combat!";
+}
 int main()
 {
     std::cout << "Text based game" << std::endl;
 
     choseCharacter();
 
-    #pragma region Unit Test for stat selection
+    choseEnemy();
 
+    #pragma region Unit Test for stat selection
+    /*
     std::cout << currName << "\n";
     std::cout << currHealth << "\n";
     std::cout << currDmg << "\n";
@@ -71,6 +164,7 @@ int main()
         std::cout << currAbilities[i];
     }
     std::cout << std::endl;
+    */
     #pragma endregion
 
 }
